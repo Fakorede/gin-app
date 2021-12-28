b07f828dc46bcddfd67620737107538e22282a5b
fakorede <abiolafakorede@fmail.com>
2021-12-28 12:59:23 +0100

    html templates

--- controllers/video-controller.go
+++ controllers/video-controller.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/Fakorede/gin-app/entity"
 	"github.com/Fakorede/gin-app/services"
 	"github.com/Fakorede/gin-app/validators"
@@ -11,6 +13,7 @@ import (
 type VideoController interface {
 	FindAll() []entity.Video
 	Save(ctx *gin.Context) error
+	ShowAll(ctx *gin.Context)
 }
 
 type videoController struct {
@@ -46,4 +49,14 @@ func (c *videoController)	Save(ctx *gin.Context) error {
 
 	c.service.Save(video)
 	return nil
+}
+
+func (c *videoController) ShowAll(ctx *gin.Context) {
+	videos := c.service.FindAll()
+	data := gin.H{
+		"title": "All Videos",
+		"videos": videos,
+	}
+
+	ctx.HTML(http.StatusOK, "index.html", data)
 }
